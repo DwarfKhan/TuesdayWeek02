1659a5f927c503aaa4bcd7e0b71d8fdf677a6f52
Unknown <skmiscool@gmail.com>
2018-01-29 16:58:35 -0800

    Ai searches for nearest Ball

--- Assets/Scrips/AIController.cs
+++ Assets/Scrips/AIController.cs
@@ -2,25 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class AIController : MonoBehaviour {
 
     public Paddle m_Paddle;
-    public Transform m_FollowTarget;
+    public Ball m_DefaultBall;
+    private Transform m_FollowTarget;
     public Vector3 m_Offset = Vector3.zero;
     public float m_RefTypeTimer;
     public float m_RefTypeTime = 3;
     public float m_OffsetTimer;
     public float m_OffsetTime = 3;
+    List<Ball> m_Balls = new List<Ball>();
 
     // Use this for initialization
     void Start()
     {
-
+        m_Balls.Add(m_DefaultBall);
+        //m_FollowTarget = m_DefaultBall.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+        m_FollowTarget = FindClosestBall().transform;
+        
         ChooseType();
         Offset();
         float diff = (m_FollowTarget.position.y + m_Offset.y) - transform.position.y;
@@ -58,12 +64,30 @@ public class AIController : MonoBehaviour {
         m_OffsetTimer -= Time.deltaTime;
         if (m_OffsetTimer < 0)
         {
-        m_Offset.x = Random.Range(-5.0f, 5.0f);
         m_Offset.y = Random.Range(-5.0f, 5.0f);
             Debug.Log(m_Offset);
             m_OffsetTimer = m_OffsetTime;
         }
 
+    }
+
+    Ball FindClosestBall()
+    {
+        float lowestDist = 0f;
+        int index = 0;
+        float currentDist;
+        for (int i = 0; i < m_Balls.Count; ++i)
+        {
+
+            Vector3 difference = m_Paddle.transform.position - m_Balls[i].transform.position;
+            currentDist = difference.magnitude;
+            if (lowestDist > currentDist)
+            {
+                index = i;
+                lowestDist = currentDist;
+            }
+        }
+        return m_Balls[index];
     }
 
 }
--- Assets/Scrips/Ball.cs
+++ Assets/Scrips/Ball.cs
@@ -7,17 +7,21 @@ using UnityEngine.UI;
 public class Ball : MonoBehaviour {
 
     public Rigidbody m_Rigidbody;
-     float m_Speed;
+
+    public float m_StartSpeed;
+    float m_Speed;
     float m_DisplaySpeed;
-    float m_multi = 1f;
+    float m_Multi = 1f;
     float m_StuckTimer;
     float m_StuckTime = 5f;
-    float m_speedCreep = 1.07f;
+    float m_SpeedCreep = 1.07f;
     float m_SpikeSpeedCreep = .2f;
-    public float m_StartSpeed;
+
     public Text txtSpeed;
     public GameObject trail;
+    public GameObject m_Ball;
     bool m_Spiked = false;
+    public bool m_IsSplitBall;
 
     Vector3 m_Orientation;
     Vector3 startPos;
@@ -25,14 +29,14 @@ public class Ball : MonoBehaviour {
     // Use this for initialization
     void Start () {
         startPos = transform.position;
-        Respawn();        
+        Respawn(true);        
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Unstuck();
         Particle();
-        m_DisplaySpeed = m_Speed * m_multi;
+        m_DisplaySpeed = m_Speed * m_Multi;
         txtSpeed.text = m_DisplaySpeed.ToString();
 	}
 
@@ -56,15 +60,15 @@ public class Ball : MonoBehaviour {
             {
                 m_Orientation.y = 0;
                 m_Orientation = m_Orientation.normalized;
-                m_multi += m_SpikeSpeedCreep;
+                m_Multi += m_SpikeSpeedCreep;
             }
             else
             {
-                m_multi = 1f;
+                m_Multi = 1f;
             }
 
-            m_Speed *= m_speedCreep;
-            m_Rigidbody.AddForce(m_Orientation.normalized * m_Speed * m_multi);
+            m_Speed *= m_SpeedCreep;
+            m_Rigidbody.AddForce(m_Orientation.normalized * m_Speed * m_Multi);
 
         }
         else if(missile != null){
@@ -86,11 +90,15 @@ public class Ball : MonoBehaviour {
         //    audioSource.Play();
     }
 
-    void Respawn()
+    void Respawn(bool startGame = false)
     {
+        if (m_IsSplitBall)
+        {
+            m_Ball.SetActive(startGame);
+        }
         transform.position = startPos;
         m_Speed = m_StartSpeed;
-        m_multi = 1f;
+        m_Multi = 1f;
         Vector3 startOrientation = Vector3.zero;
         startOrientation.x = Random.Range(-1.0f, 1.0f);
         startOrientation.y = Random.Range(-.5f, .5f);
@@ -115,11 +123,11 @@ public class Ball : MonoBehaviour {
     {
         if (Mathf.Abs(m_Orientation.y) > 0)
         {
-            m_multi = 1;
+            m_Multi = 1;
         }
         else
         {
-            m_multi+= 0.1f;
+            m_Multi+= 0.1f;
         }
     }
 
